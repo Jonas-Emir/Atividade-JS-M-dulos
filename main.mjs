f355abe0bee28199572dc5ff93122b9e9d1c7f9b
Jonas Emir <jonasemir00@gmail.com>
2022-04-18 19:36:16 -0300

    first commit

---
+++ main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from './funcoes.mjs'
+
+console.log(mostraIdade('Jonas, 23'))
+console.log(mostraCidade('Jonas, Joinville'))
+console.log(mostraHobby('Jonas, ler livros'))
